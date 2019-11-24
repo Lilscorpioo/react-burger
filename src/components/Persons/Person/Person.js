db1fadcd707eabe25e51bac5106b09fac872d100
Lilscorp <iris.lilscorp@gmail.com>
2019-11-25 02:41:38 +0300

    trying to use Aux.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxilliary';
 import classes from './Person.css';
 
 class Person extends Component { 
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>{this.props.name} is a Person! {this.props.age} years old! {this.props.children}</p>  
-        <input type="text" onChange={this.props.changed} value={this.props.name} />  
-      </div>
+      <Aux>
+        <p key="i1" onClick={this.props.click}>{this.props.name} is a Person! {this.props.age} years old!</p>,
+        <p key="i2"> {this.props.children}</p>,  
+        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />  
+      </Aux>
     );
   }  
 };
