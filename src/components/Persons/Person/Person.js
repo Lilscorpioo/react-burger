2d10dc8b695e9a0a54d71289f820c156cb85926f
Lilscorp <iris.lilscorp@gmail.com>
2020-01-18 23:03:26 +0300

    finished section 7

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,15 +3,39 @@ import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
+import AuthContext from '../../../context/auth-context';
 
-class Person extends Component { 
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
       <Aux>
+        {this.context.authenticated ? 
+          <p>Authenticated!</p> : 
+          <p>Please log in</p>
+        }
         <p key="i1" onClick={this.props.click}>{this.props.name} is a Person! {this.props.age} years old!</p>
         <p key="i2"> {this.props.children}</p>  
-        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />  
+        <input key="i3"
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />  
       </Aux>
     );
   }  
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import withClass from '../hoc/withClass';
 import Aux from '../hoc/Auxiliary';
+import AuthContext from '../context/auth-context';
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +21,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -76,29 +78,43 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  };
+
   render () {
     console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = 
+      persons = (
         <Persons 
-        persons={this.state.persons}
-        clicked={this.deletePersonHandler}
-        changed={this.nameChangedHandler} />
-      ;
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
+        />
+      );
     }
 
     return (
         <Aux>
           <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
-          {this.state.showCockpit ? <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          personsLength={this.state.persons.length}
-          clicked={this.togglePersonsHandler} 
-          /> : null}
+          <AuthContext.Provider 
+            value = {{
+              authenticated: this.state.authenticated, 
+              login: this.loginHandler
+            }}>
+            {this.state.showCockpit ?
+            (<Cockpit
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+            />
+            ) : null}
             {persons}
+          </AuthContext.Provider>
         </Aux>
     ); 
   } 
